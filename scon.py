65f179d5f7e203d7f25cfbaf58a89368d39fa19f
Jona <jalvarez@tamarindolabs.com>
2012-10-30 18:05:49 -0500

    Validacion tamaño de imagen y opcionalidad para guardar imagen binarizada

--- scon.py
+++ scon.py
@@ -15,61 +15,52 @@ import serial
 import sys
 import Image
 
-# ---------------
-# IMG TO BINARY
-# ---------------
 
-def img2bin( imgpath, imgout ) :
-	try:
-	    im = Image.open(imgpath)	    
-	except IOError:
-	    print("Image file not found")
-	else:
-		x,y = im.size
-
-		for i in range(x):
-			for j in range(y):
-				r,g,b = im.getpixel((i,j))
-				if( (r+g+b)/3 < 125 ) :
-					im.putpixel((i,j),(0,0,0))
-				else:
-					im.putpixel((i,j),(255,255,255))
-		if imgout: im.save(imgout)
-
-# ---------------
-# SERIAL CONECTION
-# ---------------
+# -----------------------------------
+# SERIAL CONECTION & IMG TO BINARIZE
+# -----------------------------------
 def main() :
+
+	# Configuración del Puerto
 	ser = serial.Serial('COM1')
 	ser.baudrate = 115200
 	ser.bytesize = serial.EIGHTBITS
 	ser.parity = serial.PARITY_NONE
 	ser.stopbits = serial.STOPBITS_ONE
 	ser.xonxoff = False
+	saveimg = False
 
 	if not ser.isOpen():
 		ser.open()
 
 	if len(sys.argv) > 1 :
+		if len(sys.argv) >= 3 : saveimg = True
+
 		try:
-			#abrir imagen
 			im = Image.open(sys.argv[1])
 		except IOError:
 		    print("Image file not found")
 		else:
 			x,y = im.size
 
-			for i in range(x):
-				for j in range(y):
-					r,g,b = im.getpixel((i,j))
-					if( (r+g+b)/3 < 125 ) :
-						im.putpixel((i,j),(0,0,0))
-						ser.write('0')
-					else:
-						im.putpixel((i,j),(255,255,255))
-						ser.write('1')
+			if x <= 200 or y <= 200 :
+				for i in range(x):
+					for j in range(y):
+						r,g,b = im.getpixel((i,j))
+						if( (r+g+b)/3 < 125 ) :
+							if(saveimg): im.putpixel((i,j),(0,0,0))
+							ser.write('0')
+						else:
+							if(saveimg): im.putpixel((i,j),(255,255,255))
+							ser.write('1')
+				ser.close()
 
-			ser.close()
+				if saveimg :
+					try: im.save( sys.argv[2] )
+					except Exception, e: raise e
+			else :
+				print( "Image Dimensions must be less than 200x200px" )
+			
 	else :
 		print( "Image File argument expected" )
 
